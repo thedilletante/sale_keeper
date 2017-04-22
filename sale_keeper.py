6d97d54dee927d470ce721cd5e9dfb02f25f8461
Mikhail Pokhikhilov <dilletante@yandex-team.ru>
2017-04-22 22:19:08 +0300

    added naive config

--- sale_keeper.py
+++ sale_keeper.py
@@ -1,27 +1,38 @@
 from flask import Flask, request
-from sys import argv
 import telepot
 import urllib3
+import json
 
-proxy_url = "http://proxy.server:3128"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url,
-                                    num_pools=3,
-                                    maxsize=10,
-                                    retries=False,
-                                    timeout=30),
-}
-telepot.api._onetime_pool_spec =\
-    (urllib3.ProxyManager,
-     dict(proxy_url=proxy_url,
-          num_pools=1,
-          maxsize=1,
-          retries=False,
-          timeout=30))
+with open("config.json") as config:
+    jconf = json.load(config)
+    if not "secret" in jconf or not "token" in jconf or not "host" in jconf:
+        print("There is no valid configuration")
+        exit(0)
 
-secret = argv[1]
-token = argv[2]
-host = argv[3]
+    secret = jconf["secret"]
+    token = jconf["token"]
+    host = jconf["host"]
+    set_proxy = True
+
+    if "set_proxy" in jconf:
+        set_proxy = jconf["set_proxy"]
+
+if set_proxy:
+    proxy_url = "http://proxy.server:3128"
+    telepot.api._pools = {
+        'default': urllib3.ProxyManager(proxy_url=proxy_url,
+                                        num_pools=3,
+                                        maxsize=10,
+                                        retries=False,
+                                        timeout=30),
+    }
+    telepot.api._onetime_pool_spec =\
+        (urllib3.ProxyManager,
+         dict(proxy_url=proxy_url,
+              num_pools=1,
+              maxsize=1,
+              retries=False,
+              timeout=30))
 
 bot = telepot.Bot(token)
 bot.setWebhook("{}/{}".format(host, secret), max_connections=1)
