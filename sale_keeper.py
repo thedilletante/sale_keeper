462b9969aaac57be3ef2280f9d0e760c4b1806ad
Mikhail Pokhikhilov <dilletante@yandex-team.ru>
2017-04-22 22:08:19 +0300

    Removed secrets and fixed formatting

--- sale_keeper.py
+++ sale_keeper.py
@@ -1,18 +1,33 @@
 from flask import Flask, request
+from sys import argv
 import telepot
 import urllib3
 
 proxy_url = "http://proxy.server:3128"
 telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+    'default': urllib3.ProxyManager(proxy_url=proxy_url,
+                                    num_pools=3,
+                                    maxsize=10,
+                                    retries=False,
+                                    timeout=30),
 }
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+telepot.api._onetime_pool_spec =\
+    (urllib3.ProxyManager,
+     dict(proxy_url=proxy_url,
+          num_pools=1,
+          maxsize=1,
+          retries=False,
+          timeout=30))
 
-secret = "64b01068-bf3c-4b1c-9426-bac7a1e3ab66"
-bot = telepot.Bot('345081096:AAHt-Mx3_hlc25ZBA5u0xOd9YelC_kyGKDs')
-bot.setWebhook("https://dilletante.pythonanywhere.com/{}".format(secret), max_connections=1)
+secret = argv[1]
+token = argv[2]
+host = argv[3]
+
+bot = telepot.Bot(token)
+bot.setWebhook("{}/{}".format(host, secret), max_connections=1)
 
 app = Flask(__name__)
+
 
 @app.route('/{}'.format(secret), methods=["POST"])
 def telegram_webhook():
